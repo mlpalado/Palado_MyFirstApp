3ad2364ecf4796b89854db2541d74bedf032b064
mlpalado <yukinery123@gmail.com>
2022-01-10 22:59:38 +0800

    With Music

--- app/src/main/java/mcm/edu/ph/palado_myfisrtapp/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/palado_myfisrtapp/MainActivity.java
@@ -2,13 +2,24 @@ package mcm.edu.ph.palado_myfisrtapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaParser;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer player;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        player = new MediaPlayer();
+        player = MediaPlayer.create(this, R.raw.silhouette);
+
+        player.start();
+
+
     }
 }
