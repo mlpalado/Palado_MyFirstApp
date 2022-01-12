13d9bee1448e7a647e58357bf0533a007886418b
mlpalado <yukinery123@gmail.com>
2022-01-12 11:51:59 +0800

    sowrd

---
+++ app/src/main/java/mcm/edu/ph/palado_myfisrtapp/sowrd.java
@@ -0,0 +1,30 @@
+package mcm.edu.ph.palado_myfisrtapp;
+
+public class sowrd {
+
+    int damage; // it means you are asking your ram to reserve you 32 bits of memory
+    int sharpness;
+    double price;
+    boolean isItDamaged;
+
+
+    int month = 12;
+    int age = 150;
+    int year 2022
+
+    float priceprice = 0.0f;
+    double prays = 0.0d;
+    double praise = 0.999;
+    double x;
+
+
+
+    boolean gwapoKo =false;
+    boolean cold = true;
+
+    char letter = ' ';  //16bits
+
+
+    //void stab(){}
+    //void stash(){}
+}
